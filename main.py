fc561f8df1d15de661150c92ca586721ca825481
Yimo Awanardo <awanardo@protonmail.ch>
2019-11-30 16:37:58 +0100

    Added structure of main program file

---
+++ main.py
@@ -0,0 +1,39 @@
+# Cow Bull Game
+# 1 cow = 1 misplaced number
+# 1 bull = 1 well placed number
+# example of numbers : "0123" "0012" "1035" "9959"
+
+from random import randint
+
+
+
+class CowBull:
+    def rand_num(self):
+        num = ""
+        for i in range(self.num_digits):
+            num += str(randint(0, 9))
+        return num
+
+    def __init__(self, num_digits=4):  # num_digits for the number of digits in the number to guess
+        self.num_digits = num_digits
+        self.number = self.rand_num()
+
+    def reset_number(self):
+        self.number = self.rand_num()
+
+    def evaluate(self, proposition):  # evaluates a proposition, returns number of cows and number of bulls
+        cows = 0
+        bulls = 0
+
+        number_copy = self.number  # we copy it because we will delete from it during the evaluation.
+        proposition_copy = proposition  # same for the proposition
+
+        # Step 1 : find the well-placed numbers, count them, then "delete" them
+        for i in range(self.num_digits):
+            if number_copy[i] == proposition_copy[i]:
+                number_copy[i] = "n"  # Replace all found well placed numbers by "n" so we don't recheck them afterwards
+                proposition_copy[i] = "n"  # Do the same thing
+
+
+game = CowBull()
+print(game.number)
